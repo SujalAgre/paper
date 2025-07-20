15ec716df57594cabc852dffbcdd9d9caaca5347
SujalAgre <sujalagre26@gmail.com>
2025-07-20 23:58:23 +0530

    fix localstorage might return null error

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Sidebar from "@/components/Sidebar";
 import { useState } from "react";
 import { useEffect } from "react";
 type message = {
-  role: "user" | "assistant" ;
+  role: "user" | "assistant";
   content: string | null;
 }
 
@@ -23,21 +23,23 @@ export default function Home() {
   const [chatObject, setChatObject] = useState<chat[]>([]);
   const [currentChatId, setCurrentChatId] = useState<string | null>(null);
 
-  useEffect(()=>{
-    const storedChatObject = JSON.parse(localStorage.getItem('chatObject'))
-    setChatObject(storedChatObject)
-    console.log(chatObject)
-  },[])
+  useEffect(() => {
+    const storedChatObject = JSON.parse(localStorage.getItem('chatObject')!)
+    if (storedChatObject) {
+      setChatObject(storedChatObject)
+      console.log(chatObject)
+    }
+  }, [])
 
-  useEffect(()=>{
+  useEffect(() => {
     localStorage.setItem('chatObject', JSON.stringify(chatObject))
-  },[chatObject])
+  }, [chatObject])
 
   return (
     <>
       <div className="flex">
         {sidebar && <div className="border-r-1 h-screen w-80 border-[#d9d9d9]">
-        <Sidebar setSidebar={setSidebar} chatObject={chatObject} setChatObject={setChatObject} setMessages={setMessages} currentChatId={currentChatId} setCurrentChatId={setCurrentChatId}/>
+          <Sidebar setSidebar={setSidebar} chatObject={chatObject} setChatObject={setChatObject} setMessages={setMessages} currentChatId={currentChatId} setCurrentChatId={setCurrentChatId} />
         </div>}
 
         <div className="w-full">
